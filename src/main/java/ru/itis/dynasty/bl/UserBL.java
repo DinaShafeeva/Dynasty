15daa9f8511ab0ac70f048bf4ecd50b780205bf6
Dina Shafeeva <di.shafeeva@yandex.ru>
2019-12-14 20:20:54 +0300

    Created UserBL, improved User

---
+++ src/main/java/ru/itis/dynasty/bl/UserBL.java
@@ -0,0 +1,91 @@
+package ru.itis.dynasty.bl;
+
+import ru.itis.dynasty.dao.UserDAO;
+import ru.itis.dynasty.dao.UserStatDAO;
+import ru.itis.dynasty.models.User;
+import ru.itis.dynasty.models.UserStat;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class UserBL {
+    private UserDAO userDAO = new UserDAO();
+    private UserStatDAO userStatDAO = new UserStatDAO();
+
+    public boolean checkPassword(String password1, String password2){
+        if ((password1.length() > 6)&&(password1.equals(password2))){
+            return true;
+        } else return false;
+    }
+
+    public User update(User user, String repeatPassword) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
+        User foundUser = userDAO.read(user.getId());
+        User newUser = new User();
+        newUser.setId(user.getId());
+
+        if (foundUser != null){
+            if (user.getName() == null){
+                newUser.setName(foundUser.getName());
+            } else {
+                newUser.setName(user.getName());
+            }
+
+            if (user.getPassword() == null){
+                newUser.setPassword(foundUser.getPassword());
+            } else {
+                if (checkPassword(user.getPassword(), repeatPassword)) {
+                    newUser.setPassword(getHash(user.getPassword()));
+                }
+            }
+
+            user = userDAO.update(newUser);
+            return user;
+        } else{
+            return foundUser;
+        }
+    }
+
+    public boolean register(String name, String password, String repeatPassword) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        if (checkPassword(password, repeatPassword)){
+            if (!userDAO.isExist(name)) {
+                User user = new User(name, getHash(password));
+                User createdUser = userDAO.create(user);
+                if (createdUser != null) {
+                    UserStat userStat = new UserStat();
+                    userStat.setIdUser(createdUser.getId());
+                    userStatDAO.create(userStat);
+                    //при нажатии на кнопку регистрации отправляет на страницу авторизации
+                    return true;
+                } else {
+                    //если уже зарегистрирован
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public User login(String name, String password) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        return userDAO.isExist(name, getHash(password)) ;
+    }
+
+
+    public String getHash(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.reset();
+        m.update(str.getBytes("utf-8"));
+        String s2 = new BigInteger(1, m.digest()).toString(16);
+        StringBuilder sb = new StringBuilder(32);
+        for (int i = 0, count = 32 - s2.length(); i < count; i++) {
+            sb.append("0");
+        }
+        return sb.append(s2).toString();
+    }
+}
+
--- src/main/java/ru/itis/dynasty/models/User.java
+++ src/main/java/ru/itis/dynasty/models/User.java
@@ -16,6 +16,9 @@ public class User {
         this.cards = cards;
     }
 
+    public User() {
+    }
+
     public User(String name, String password) {
         this.name = name;
         this.password = password;
